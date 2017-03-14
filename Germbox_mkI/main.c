ab3795edde4a0f8581a5297fff033015c67c28a8
Matjaz Tome <matjaz.tome@siworks.eu>
2017-03-14 22:17:11 +0100

    Project created in AS

---
+++ Germbox_mkI/main.c
@@ -0,0 +1,21 @@
+/*
+ * Germbox_mkI.c
+ *
+ * Created: 14. 03. 2017 22:16:34
+ * Author : matja
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
