49b336fe14ae9a167759da678e0353c312cbf671
Matjaz Tome <matjaz.tome@siworks.eu>
2017-03-15 02:57:41 +0100

    HEAting vontroll with PWM kinda works. There are some prblems with writing in new vaule

---
+++ Germbox_MK_I/src/heater.h
@@ -0,0 +1,17 @@
+/*
+ * heater.h
+ *
+ * Created: 15. 03. 2017 02:13:04
+ *  Author: matja
+ */ 
+
+
+#ifndef HEATER_H_
+#define HEATER_H_
+
+
+void heater_init(void);
+void heater_set (uint8_t heat);
+
+
+#endif /* HEATER_H_ */
--- Germbox_MK_I/src/main.c
+++ Germbox_MK_I/src/main.c
@@ -30,9 +30,9 @@
  */
 #include <asf.h>
 #include <stdint.h>
-#include "pwm.h"
-#include "pio.h"
-#include "pmc.h"
+
+#include "heater.h"
+
 #include "adc.h"
 #include "wdt.h"
 
@@ -41,48 +41,21 @@
 int main (void)
 {
 
-	pwm_channel_t pwm_ch_instance;
-	pwm_clock_t pwm_clk;
+	
 		
 	
 	sysclk_init();
 	board_init();
+	heater_init();
 	
-	
-	pmc_enable_periph_clk(ID_PIOA);
-	pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA2);
-	
-	pmc_enable_periph_clk(ID_PIOA);
-	pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA30);
-	//pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA1);
-	
-	
-	pmc_enable_periph_clk(ID_PWM);
+	heater_set(32);
 
-	pwm_channel_disable(PWM, PWM_CHANNEL_2);
-	pwm_clk.ul_clka = 1000 * 100;
-	pwm_clk.ul_clkb = 0;
-	pwm_clk.ul_mck = 32000000;
-	pwm_init(PWM, &pwm_clk);
 	
 	
-	
-	pwm_ch_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
-	pwm_ch_instance.ul_period = 100;
-	pwm_ch_instance.ul_duty = 70;
-	pwm_ch_instance.channel = PWM_CHANNEL_2;
-	pwm_ch_instance.alignment = PWM_ALIGN_LEFT;
-	pwm_ch_instance.polarity = PWM_LOW;
-	//pwm_ch_instance.b_pwml_output_inverted = true;
-	pwm_ch_instance.b_deadtime_generator = true;
-	pwm_ch_instance.us_deadtime_pwmh = 2;
-	pwm_ch_instance.us_deadtime_pwml = 2;
-	pwm_channel_init(PWM, &pwm_ch_instance);
-	
-	pwm_channel_enable(PWM, PWM_CHANNEL_2);
 	
 	while(1)
 	{
+		
 		asm("nop");
 	}
 	
