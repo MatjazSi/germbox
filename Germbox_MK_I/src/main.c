bb29059c26bae3fdce336f6b119b8845554d75db
Matjaz Tome <matjaz.tome@siworks.eu>
2017-03-15 02:11:17 +0100

    PWM working in complementary mode

--- Germbox_MK_I/src/main.c
+++ Germbox_MK_I/src/main.c
@@ -48,28 +48,42 @@ int main (void)
 	sysclk_init();
 	board_init();
 	
-	pmc_enable_periph_clk(ID_PWM);
+	
 	pmc_enable_periph_clk(ID_PIOA);
-	pwm_channel_disable(PWM, PWM_CHANNEL_0);
+	pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA2);
+	
+	pmc_enable_periph_clk(ID_PIOA);
+	pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA30);
+	//pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA1);
+	
+	
+	pmc_enable_periph_clk(ID_PWM);
+
+	pwm_channel_disable(PWM, PWM_CHANNEL_2);
 	pwm_clk.ul_clka = 1000 * 100;
 	pwm_clk.ul_clkb = 0;
 	pwm_clk.ul_mck = 32000000;
 	pwm_init(PWM, &pwm_clk);
 	
-	pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA0);
-	pio_set_peripheral(PIOA, PIO_TYPE_PIO_PERIPH_A, PIO_PA1);
+	
 	
 	pwm_ch_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
 	pwm_ch_instance.ul_period = 100;
-	pwm_ch_instance.ul_duty = 50;
-	pwm_ch_instance.channel = PWM_CHANNEL_0;
+	pwm_ch_instance.ul_duty = 70;
+	pwm_ch_instance.channel = PWM_CHANNEL_2;
+	pwm_ch_instance.alignment = PWM_ALIGN_LEFT;
+	pwm_ch_instance.polarity = PWM_LOW;
+	//pwm_ch_instance.b_pwml_output_inverted = true;
+	pwm_ch_instance.b_deadtime_generator = true;
+	pwm_ch_instance.us_deadtime_pwmh = 2;
+	pwm_ch_instance.us_deadtime_pwml = 2;
 	pwm_channel_init(PWM, &pwm_ch_instance);
 	
-	pwm_channel_enable(PWM, PWM_CHANNEL_0);
+	pwm_channel_enable(PWM, PWM_CHANNEL_2);
 	
 	while(1)
 	{
-		
+		asm("nop");
 	}
 	
 	
