e03b701404429bc35ab86ad16b6ac1a873f8d8d0
Matjaz Tome <matjaz.tome@siworks.eu>
2017-03-15 23:49:17 +0100

    Master commit

---
+++ Germbox_MK_I/src/heater.c
@@ -0,0 +1,54 @@
+/*
+ * heater.c
+ *
+ * Created: 15. 03. 2017 02:12:48
+ *  Author: matja
+ */ 
+
+
+#include "pwm.h"
+#include "pio.h"
+#include "pmc.h"
+
+#include "hardware_conf.h"
+
+#include <stdint.h>
+
+pwm_channel_t pwm_ch_instance;
+
+void heater_init(void)
+{
+	
+	pwm_clock_t pwm_clk;
+	
+	pmc_enable_periph_clk(ID_PIOA);
+	pio_set_peripheral(HEATER_PORT, HEATER_PIN_MUX, HEATER_PIN);
+	
+	pmc_enable_periph_clk(ID_PWM);
+	pwm_channel_disable(PWM, HEATER_PWM_CH);
+	pwm_clk.ul_clka = HEATER_PWM_FREQUENCY * 100;
+	pwm_clk.ul_clkb = 0;
+	pwm_clk.ul_mck = 32000000;
+	pwm_init(PWM, &pwm_clk);
+	
+	
+	
+	pwm_ch_instance.ul_prescaler = PWM_CMR_CPRE_CLKA;
+	pwm_ch_instance.ul_period = 100;
+	pwm_ch_instance.ul_duty = 3;
+	pwm_ch_instance.channel = HEATER_PWM_CH;
+	pwm_ch_instance.alignment = PWM_ALIGN_LEFT;
+	pwm_ch_instance.polarity = PWM_LOW;
+	pwm_channel_init(PWM, &pwm_ch_instance);
+	pwm_channel_enable(PWM, HEATER_PWM_CH);
+	
+}
+
+volatile void heater_set (uint8_t heat)
+{
+	if(heat > 100)
+	{
+		heat = 100;
+	}
+	pwm_channel_update_duty(PWM, &pwm_ch_instance, (uint32_t)heat);
+}
--- Germbox_MK_I/src/main.c
+++ Germbox_MK_I/src/main.c
@@ -29,12 +29,37 @@
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 #include <asf.h>
+#include <stdint.h>
+
+#include "heater.h"
+
+#include "adc.h"
+#include "wdt.h"
+
+
 
 int main (void)
 {
-	/* Insert system clock initialization code here (sysclk_init()). */
 
+	
+		
+	
+	sysclk_init();
 	board_init();
+	heater_init();
+	
+	heater_set(32);
+
+	
+	
+	
+	while(1)
+	{
+		
+		asm("nop");
+	}
+	
+	
 
 	/* Insert application code here, after the board has been initialized. */
 }
