0707234d9703f65a13cf2f43dd77e7cfb7433994
Matjaz Tome <matjaz.tome@siworks.eu>
2017-03-20 00:56:45 +0100

    Start of thermo branch

---
+++ Germbox_MK_I/src/thermo.c
@@ -0,0 +1,6 @@
+/*
+ * thermo.c
+ *
+ * Created: 20. 03. 2017 00:51:53
+ *  Author: matja
+ */ 
---
+++ Germbox_MK_I/src/thermo.h
@@ -0,0 +1,16 @@
+/*
+ * thermo.h
+ *
+ * Created: 20. 03. 2017 00:52:16
+ *  Author: matja
+ */ 
+
+
+#ifndef THERMO_H_
+#define THERMO_H_
+
+
+
+
+
+#endif /* THERMO_H_ */
