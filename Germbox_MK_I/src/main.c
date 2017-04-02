2c88afcacda7453d5d5abb829620c2615e175976
Matjaz Tome <matjaz.tome@siworks.eu>
2017-04-02 23:21:55 +0200

    Added support for watering and timing related to watering

--- Germbox_MK_I/src/main.c
+++ Germbox_MK_I/src/main.c
@@ -39,22 +39,33 @@
 #include "display.h"
 #include "heater.h"
 #include "thermo.h"
+#include "pump.h"
 #include "encoder.h"
 
 
-#define PID_TIMER	0 // stimer channel number
-#define PRINT_TIMER	1 // timer for printing things over usb
+#define PID_TIMER		0 // stimer channel number
+#define PRINT_TIMER		1 // timer for printing things over usb
+#define MIN_TIMER		2 // timer used for minute counting
+#define WATERING_TIMER	3 // timer used for measuring 
 
 
-#define KP	30//55
-#define KI	0.05//0.06
-#define KD	90//15
+#define KP	35//55
+#define KI	0.011//0.06
+#define KD	100//15
 
 #define  I_MAX 400
 
-#define DEFAULT_SET_T	29.0
+#define DEFAULT_SET_T			29.0	//default set temperature  in °C
+#define DEFAULT_SET_HUMID		3		//defeaut set humidity
+#define DEFAULT_WATERING_TIME	10		//default duration of watering in seconds
 
-//#define PRINT_OVER_USB //uncoment if you want controller to prnt data out over USB
+#define WATTERING_INTERVAL		120 //watering interval in minutes
+#define SHORT_WATERING			15 //watering duration in seconds
+#define LONG_WATERING			25 //watering duration in seconds
+#define HUMID_TRESHOLD			4 // treshild between short and long wattering duration
+
+
+#define PRINT_OVER_USB //uncoment if you want controller to prnt data out over USB
 
 /* best so far (PI)
 #define KP	90
@@ -86,8 +97,50 @@
 
 
 
-	volatile uint32_t time_expired = 0;
-	volatile uint32_t ttp = 0;		// time to print
+volatile uint32_t time_expired = 0, watering_expired = 0; // indicator flags
+volatile uint32_t ttp = 0;		// time to print
+
+volatile uint32_t expired_time = 0;
+
+void min_count (void)
+{
+	static uint32_t water_timer = 0;
+	water_timer++;
+	expired_time++;
+	
+	if(water_timer == WATTERING_INTERVAL)
+	{
+		water_timer = 0;
+		watering_expired = 1;
+	}
+}
+
+static void water_off (void)
+{
+	pump_off;
+}
+
+
+static void init_min_counter (void)
+{
+	stimer_set_time(MIN_TIMER, 60000, 1);
+	stimer_register_callback(MIN_TIMER, min_count);
+	stimer_start(MIN_TIMER);
+}
+
+static void init_watering_timer (void)
+{
+	stimer_set_time(WATERING_TIMER, DEFAULT_WATERING_TIME * 100, 0);
+	stimer_register_callback(WATERING_TIMER, water_off);
+}
+
+void water (uint8_t amount)
+{
+	stimer_set_time(WATERING_TIMER, amount * 1000, 0);
+	pump_on; // pump will be turned off in callback routine striggered automaticaly by timer
+	stimer_start(WATERING_TIMER);
+	
+}
 
 void float_split (float input, uint32_t* wh, uint32_t* dec)
 {
@@ -111,7 +164,7 @@ int main (void)
 {
 
 	float temp, power, set_temp = DEFAULT_SET_T, old_set_temp = 0;
-	uint8_t str_pos = 0, humid;
+	uint8_t str_pos = 0, humid, set_humid = DEFAULT_SET_HUMID;
 	uint32_t whole, decimal;
 	int32_t pulses;
 	uint8_t str[20];
@@ -119,6 +172,8 @@ int main (void)
 	
 	sysclk_init();
 	board_init();
+	init_min_counter(); // start minute timer
+	init_watering_timer();
 	
 	#ifdef PRINT_OVER_USB	
 		udc_start();
@@ -154,6 +209,25 @@ int main (void)
 			time_expired = 0;
 			
 		}
+		if(watering_expired)  // time to check if wattering is needed
+		{
+			if(set_humid)
+			{
+				if(humid < set_humid)
+				{
+					if((int8_t)(set_humid - humid) < HUMID_TRESHOLD)
+					{
+						water(SHORT_WATERING);
+					}
+					else
+					{
+						water(LONG_WATERING);
+					}
+				}
+			}
+			watering_expired = 0;	
+		}
+		
 		#ifdef PRINT_OVER_USB
 		if(ttp) // time to print out things over USB
 		{
